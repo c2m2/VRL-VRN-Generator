5a1a88883c5a8a89504c4e54e2984d834ba95269
stephan <stephan@syntaktischer-zucker.de>
2020-11-10 15:10:02 +0100

    Adding another function: simulate(...).

--- VRL-VRN-Generator/src/main/java/edu/gcsc/vrl/vr/MeshGenerator.java
+++ VRL-VRN-Generator/src/main/java/edu/gcsc/vrl/vr/MeshGenerator.java
@@ -25,9 +25,20 @@ public class MeshGenerator implements Serializable {
   (
     @ParamInfo(name = "Input file", style="load-dialog") File file,
     @ParamInfo(name = "Inflation factor 2D", options="value=1.0") double inflation,
-    @ParamInfo(name = "Number of 1D Refinement", options="value=0") int refinement
+    @ParamInfo(name = "Number of 1D Refinement", options="value=0;min=0,max=10", style="slider") int refinement
   )
   {
-    /// TODO: Add ug calls to neuro_collections grid generation
+    /// TODO: Add UG API calls to neuro_collections grid generation
+  }
+
+  @MethodInfo(name="Run simulation", hide=true, num=1)
+  public void simulate
+  (
+    @ParamInfo(name = "End time [s]", options="value=1.0") double endTime,
+    @ParamInfo(name = "minDt [s]", options="value=0.00001") double minDt,
+    @ParamInfo(name = "maxDt [s]", options="value=0.001") double maxDt
+  ) 
+  {
+    /// TODO: Add UG API calls to run simulation
   }
 }
