fc7f3d570aa68d27882d9532f2d5eba303ea185e
stephan <stephan@syntaktischer-zucker.de>
2021-03-18 17:07:08 +0100

    Updating MeshGenerator.

--- VRL-VRN-Generator/src/main/java/edu/gcsc/vrl/vr/MeshGenerator.java
+++ VRL-VRN-Generator/src/main/java/edu/gcsc/vrl/vr/MeshGenerator.java
@@ -14,6 +14,9 @@ import java.io.IOException;
  * @brief Mesh Generator component to generate and bundle meshes for VR in Unity
  */
 @ComponentInfo(name = "Mesh Generator", category = "VR/")
+/**
+ * @brief Mesh Generator visual component
+ */
 public class MeshGenerator implements Serializable {
   /// necessary for session serialization
   private static final long serialVersionUID = 1L;
