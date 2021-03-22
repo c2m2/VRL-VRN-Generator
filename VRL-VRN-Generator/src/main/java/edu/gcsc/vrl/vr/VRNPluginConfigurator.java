9e038870a2eb32a234cfc0ed90f423037242821f
stephan <stephan@syntaktischer-zucker.de>
2021-03-22 11:04:59 +0100

    Version bump.

--- VRL-VRN-Generator/src/main/java/edu/gcsc/vrl/vr/VRNPluginConfigurator.java
+++ VRL-VRN-Generator/src/main/java/edu/gcsc/vrl/vr/VRNPluginConfigurator.java
@@ -26,7 +26,7 @@ public class VRNPluginConfigurator extends VPluginConfigurator {
    */
   public VRNPluginConfigurator() {
     // specify the plugin name and version 
-    setIdentifier(new PluginIdentifier("VRL-VRN-Generator", "0.0.1"));
+    setIdentifier(new PluginIdentifier("VRL-VRN-Generator", "0.0.2"));
 
     // exported by using the exportPackage() method:
     exportPackage("edu.gcsc.vrl.vr");
