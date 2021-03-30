eae31e0bd31b10886151aad7fbce56ed57af5d08
stephan <stephan@syntaktischer-zucker.de>
2021-03-30 13:52:36 +0200

    Removing obsolete include.

--- VRL-VRN-Generator/src/main/java/edu/gcsc/vrl/vr/MeshGenerator.java
+++ VRL-VRN-Generator/src/main/java/edu/gcsc/vrl/vr/MeshGenerator.java
@@ -16,7 +16,6 @@ import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.nio.file.attribute.AclEntry;
 import java.nio.file.attribute.AclEntryPermission;
 import java.nio.file.attribute.AclEntryType;
